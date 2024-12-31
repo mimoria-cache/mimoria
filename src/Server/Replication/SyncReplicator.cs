30955701ec192ec0a3675d06536288e5f99d86d1
varelen <varelen@proton.me>
2024-12-31 22:48:16 +0100

    feat(Server): Add sync replicator implementation

---
+++ src/Server/Replication/SyncReplicator.cs
@@ -0,0 +1,57 @@
+﻿// SPDX-FileCopyrightText: 2024 varelen
+//
+// SPDX-License-Identifier: MIT
+
+using System.Runtime.CompilerServices;
+
+using Varelen.Mimoria.Core;
+using Varelen.Mimoria.Core.Buffer;
+using Varelen.Mimoria.Server.Bully;
+using Varelen.Mimoria.Server.Cluster;
+
+namespace Varelen.Mimoria.Server.Replication;
+
+public sealed class SyncReplicator : IReplicator
+{
+    private readonly ClusterServer clusterServer;
+    private readonly IBullyAlgorithm bullyAlgorithm;
+
+    public SyncReplicator(ClusterServer clusterServer, IBullyAlgorithm bullyAlgorithm)
+    {
+        this.clusterServer = clusterServer;
+        this.bullyAlgorithm = bullyAlgorithm;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private bool ShouldReplicate()
+        => this.bullyAlgorithm.IsLeader;
+
+    public async ValueTask ReplicateSetStringAsync(string key, string? value, uint ttlMilliseconds)
+    {
+        if (!this.ShouldReplicate())
+        {
+            return;
+        }
+
+        // TODO: Is 'Task.WhenAll' still okay if you have many secondary nodes?
+        await Task.WhenAll(this.clusterServer.Clients.Select(clusterConnection =>
+        {
+            uint requestId = clusterConnection.Value.IncrementRequestId();
+
+            IByteBuffer byteBuffer = PooledByteBuffer.FromPool(Operation.Batch, requestId);
+            byteBuffer.WriteVarUInt(1);
+            byteBuffer.WriteByte((byte)Operation.SetString);
+            byteBuffer.WriteString(key);
+            byteBuffer.WriteString(value);
+            byteBuffer.WriteUInt(ttlMilliseconds);
+            byteBuffer.EndPacket();
+
+            return clusterConnection.Value.SendAndWaitForResponseAsync(requestId, byteBuffer).AsTask();
+        }));
+    }
+
+    public void Dispose()
+    {
+        // Nothing to do
+    }
+}
