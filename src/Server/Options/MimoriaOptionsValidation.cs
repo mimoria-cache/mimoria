b47a52e2f718f1b20de9b43acfd8f46bd10312ee
varelen <varelen@proton.me>
2025-01-02 11:18:51 +0100

    feat(Server): Add IP validation to options validation

--- src/Server/Options/MimoriaOptionsValidation.cs
+++ src/Server/Options/MimoriaOptionsValidation.cs
@@ -4,24 +4,41 @@
 
 using Microsoft.Extensions.Options;
 
+using System.Net;
+
 namespace Varelen.Mimoria.Server.Options;
 
 public sealed class MimoriaOptionsValidation : IValidateOptions<MimoriaOptions>
 {
     public ValidateOptionsResult Validate(string? name, MimoriaOptions options)
     {
+        if (!IPAddress.TryParse(options.Ip, out _))
+        {
+            return ValidateOptionsResult.Fail($"'Ip' needs to be a valid IPv4 address");
+        }
+
+        if (options.Port < ushort.MinValue || options.Port > ushort.MaxValue)
+        {
+            return ValidateOptionsResult.Fail($"'Port' needs to be in range '{ushort.MinValue}-{ushort.MaxValue}' but was '{options.Port}'");
+        }
+
         if (options.Password is null)
         {
             return ValidateOptionsResult.Fail($"Mimoria section requires a 'Password' to be set");
         }
 
-        if (options.Port < ushort.MinValue || options.Port > ushort.MaxValue)
-        {
-            return ValidateOptionsResult.Fail($"Port needs to be in range '{ushort.MinValue}-{ushort.MaxValue}' but was '{options.Port}'");
-        }
-
         if (options.Cluster is not null)
         {
+            if (!IPAddress.TryParse(options.Cluster.Ip, out _))
+            {
+                return ValidateOptionsResult.Fail($"Cluster -> 'Ip' needs to be a valid IPv4 address");
+            }
+
+            if (options.Port < ushort.MinValue || options.Port > ushort.MaxValue)
+            {
+                return ValidateOptionsResult.Fail($"Cluster -> 'Port' needs to be in range '{ushort.MinValue}-{ushort.MaxValue}' but was '{options.Cluster.Port}'");
+            }
+
             if (options.Cluster.Password is null)
             {
                 return ValidateOptionsResult.Fail($"Cluster section requires a 'Password' to be set");
