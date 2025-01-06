214e4553f0d26e003fed1fe7a671ee9b254b00bd
varelen <varelen@proton.me>
2025-01-06 22:00:00 +0100

    feat(Core): Add protocol defaults class

---
+++ src/Core/ProtocolDefaults.cs
@@ -0,0 +1,10 @@
+﻿// SPDX-FileCopyrightText: 2025 varelen
+//
+// SPDX-License-Identifier: MIT
+
+namespace Varelen.Mimoria.Core;
+
+public static class ProtocolDefaults
+{
+    public const int MinPacketLength = 4;
+}
