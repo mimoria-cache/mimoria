8335ea080b2cba796c7ff63db29322312a22fd9c
varelen <varelen@proton.me>
2025-03-02 20:04:58 +0100

    feat(Server): Add async keyed locking implementation

---
+++ src/Server/Cache/Locking/AutoRemovingAsyncKeyedLocking.cs
@@ -0,0 +1,113 @@
+﻿// SPDX-FileCopyrightText: 2025 varelen
+//
+// SPDX-License-Identifier: MIT
+
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Varelen.Mimoria.Server.Cache.Locking;
+
+/// <summary>
+/// An implementation of async keyed locking based on a string key using <see cref="SemaphoreSlim"/> and <see cref="ConcurrentDictionary{TKey, TValue}"/>.
+/// 
+/// This implementation is based on the library <see href="https://github.com/MarkCiliaVincenti/AsyncKeyedLock" >AsyncKeyedLock</see> from Mark Cilia Vincenti.
+/// 
+/// MIT License
+/// 
+/// Copyright(c) 2024 Mark Cilia Vincenti
+/// Copyright(c) 2025 varelen
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in all
+/// copies or substantial portions of the Software.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+/// </summary>
+public sealed class AutoRemovingAsyncKeyedLocking : IDisposable
+{
+    private readonly ConcurrentDictionary<string, ReferenceCountedReleaser> releasersByKey;
+
+    public AutoRemovingAsyncKeyedLocking(int initialCapacity)
+        => this.releasersByKey = new(Environment.ProcessorCount, capacity: initialCapacity);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public async ValueTask<ReferenceCountedReleaser?> LockAsync(string key, bool takeLock = true)
+    {
+        if (!takeLock)
+        {
+            return null;
+        }
+
+        if (this.releasersByKey.TryGetValue(key, out ReferenceCountedReleaser? existingReleaser)
+            && existingReleaser.TryRetain())
+        {
+            return await WaitAsync(existingReleaser);
+        }
+
+        var newReleaser = new ReferenceCountedReleaser(this, key);
+        if (this.releasersByKey.TryAdd(key, newReleaser))
+        {
+            return await WaitAsync(newReleaser);
+        }
+
+        return await this.GetOrAddReleaser(key, newReleaser);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private async Task<ReferenceCountedReleaser> GetOrAddReleaser(string key, ReferenceCountedReleaser newReleaser)
+    {
+        do
+        {
+            ReferenceCountedReleaser maybeUseableReleaser = this.releasersByKey.GetOrAdd(key, newReleaser);
+            if (maybeUseableReleaser == newReleaser || maybeUseableReleaser.TryRetain())
+            {
+                return await WaitAsync(maybeUseableReleaser);
+            }
+        } while (true);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static async Task<ReferenceCountedReleaser> WaitAsync(ReferenceCountedReleaser releaser)
+    {
+        await releaser.Semaphore.WaitAsync();
+        return releaser;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Release(ReferenceCountedReleaser releaser)
+    {
+        lock (releaser.Locker)
+        {
+            if (releaser.TryLose())
+            {
+                if (this.releasersByKey.TryRemove(releaser.Key, out var removedReleaser))
+                {
+                    Debug.Assert(removedReleaser == releaser, "Removed releaser does not match releaser");
+                }
+
+                int previousCount = releaser.Semaphore.Release();
+                Debug.Assert(previousCount == 0, "Sempahore previous count was not zero");
+                return;
+            }
+        }
+
+        int outerPreviousCount = releaser.Semaphore.Release();
+        Debug.Assert(outerPreviousCount == 0, "Sempahore outer previous count was not zero");
+    }
+
+    public void Dispose()
+        => Debug.Assert(this.releasersByKey.IsEmpty, "Locks were not empty on dispose");
+}
---
+++ src/Server/Cache/Locking/ReferenceCountedReleaser.cs
@@ -0,0 +1,88 @@
+﻿// SPDX-FileCopyrightText: 2025 varelen
+//
+// SPDX-License-Identifier: MIT
+
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Varelen.Mimoria.Server.Cache.Locking;
+
+/// <summary>
+/// An implementation of a reference counted releaser for handling the automatic release of a keyed lock.
+/// </summary>
+public sealed class ReferenceCountedReleaser : IDisposable
+{
+    private readonly AutoRemovingAsyncKeyedLocking autoRemovingAsyncKeyedLocking;
+    private readonly SemaphoreSlim semaphoreSlim = new(initialCount: 1, maxCount: 1);
+    private readonly string key;
+    private readonly Lock locker = new();
+
+    private int referenceCount;
+
+    public SemaphoreSlim Semaphore
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => this.semaphoreSlim;
+    }
+
+    public string Key
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => this.key;
+    }
+
+    public Lock Locker
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => this.locker;
+    }
+
+    public ReferenceCountedReleaser(AutoRemovingAsyncKeyedLocking autoRemovingAsyncKeyedLocking, string key)
+    {
+        this.autoRemovingAsyncKeyedLocking = autoRemovingAsyncKeyedLocking;
+        this.key = key;
+        this.referenceCount = 1;
+    }
+
+    /// <summary>
+    /// Tries to retain the releaser by incrementing the reference count.
+    /// </summary>
+    /// <returns>true if retained, otherwise false.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public bool TryRetain()
+    {
+        if (this.locker.TryEnter())
+        {
+            // This releaser was already removed, so don't use it
+            if (this.referenceCount == 0)
+            {
+                this.locker.Exit();
+                return false;
+            }
+
+            this.referenceCount++;
+            this.locker.Exit();
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Decrements the reference count and returns true if the reference count reached zero.
+    /// </summary>
+    /// <returns>true if the reference count is zero (releaser can be removed), otherwise false.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public bool TryLose()
+    {
+        Debug.Assert(this.locker.IsHeldByCurrentThread);
+        return --this.referenceCount == 0;
+    }
+
+    /// <summary>
+    /// This disposes the releaser and releases the lock.
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Dispose()
+        => this.autoRemovingAsyncKeyedLocking.Release(this);
+}
