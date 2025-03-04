1da64074fc3edeae266005bb10d464e5d0fe84ca
varelen <varelen@proton.me>
2025-03-04 22:10:00 +0100

    feat(Service): Add service project with docker support

---
+++ src/Service/MimoriaHostedService.cs
@@ -0,0 +1,46 @@
+﻿// SPDX-FileCopyrightText: 2025 varelen
+//
+// SPDX-License-Identifier: MIT
+
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using Varelen.Mimoria.Server;
+
+namespace Varelen.Mimoria.Service;
+
+public sealed class MimoriaHostedService : IHostedService
+{
+    private readonly ILogger<MimoriaHostedService> logger;
+    private readonly IMimoriaServer mimoriaServer;
+
+    public MimoriaHostedService(ILogger<MimoriaHostedService> logger, IMimoriaServer mimoriaServer)
+    {
+        this.logger = logger;
+        this.mimoriaServer = mimoriaServer;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _ = StartAsyncCore();
+        return Task.CompletedTask;
+    }
+
+    public async Task StartAsyncCore()
+    {
+        try
+        {
+            await this.mimoriaServer.StartAsync();
+        }
+        catch (Exception exception)
+        {
+            this.logger.LogError(exception, "Unexpected error while starting Mimoria server");
+        }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        this.mimoriaServer.Stop();
+        return Task.CompletedTask;
+    }
+}
---
+++ src/Service/Program.cs
@@ -0,0 +1,43 @@
+﻿// SPDX-FileCopyrightText: 2025 varelen
+//
+// SPDX-License-Identifier: MIT
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using Varelen.Mimoria.Server;
+using Varelen.Mimoria.Server.Cache;
+using Varelen.Mimoria.Server.Options;
+using Varelen.Mimoria.Server.Protocol;
+using Varelen.Mimoria.Server.PubSub;
+using Varelen.Mimoria.Service;
+
+await Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+        IConfiguration configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: true)
+            .AddEnvironmentVariables()
+            .AddCommandLine(args)
+            .Build();
+
+        services.AddOptions<MimoriaOptions>()
+            .Bind(configuration.GetSection("Mimoria"))
+            .ValidateOnStart();
+        services.AddSingleton<IValidateOptions<MimoriaOptions>, MimoriaOptionsValidation>();
+
+        services.AddSingleton<IMimoriaSocketServer, MimoriaSocketServer>();
+        services.AddSingleton<IPubSubService, PubSubService>();
+#if DEBUG
+        services.AddSingleton<ICache>(serviceProvider => new LoggingCache(serviceProvider.GetRequiredService<ILogger<LoggingCache>>(), new ExpiringDictionaryCache(serviceProvider.GetRequiredService<ILogger<ExpiringDictionaryCache>>(), serviceProvider.GetRequiredService<IPubSubService>(), TimeSpan.FromMinutes(5))));
+#else
+        services.AddSingleton<ICache>(serviceProvider => new ExpiringDictionaryCache(serviceProvider.GetRequiredService<ILogger<ExpiringDictionaryCache>>(), serviceProvider.GetRequiredService<IPubSubService>(), TimeSpan.FromMinutes(5)));
+#endif
+        services.AddSingleton<IMimoriaServer, MimoriaServer>();
+
+        services.AddHostedService<MimoriaHostedService>();
+    })
+    .RunConsoleAsync();
