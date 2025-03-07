3468504ff6a76df32ca556880805a2ea77027389
varelen <varelen@proton.me>
2025-03-07 18:18:12 +0100

    feat(Core): Add new pub-sub channel for primary changed

--- src/Core/Channels.cs
+++ src/Core/Channels.cs
@@ -1,4 +1,4 @@
-﻿// SPDX-FileCopyrightText: 2024 varelen
+﻿// SPDX-FileCopyrightText: 2025 varelen
 //
 // SPDX-License-Identifier: MIT
 
@@ -6,5 +6,13 @@ namespace Varelen.Mimoria.Core;
 
 public static class Channels
 {
+    /// <summary>
+    /// Published to if a key expired. Payload is the key (string) which expired.
+    /// </summary>
     public const string KeyExpiration = "__expiration";
+
+    /// <summary>
+    /// Published to if the primary server changed. Payload is the id (int) of the new primary server.
+    /// </summary>
+    public const string PrimaryChanged = "__primary:changed";
 }
