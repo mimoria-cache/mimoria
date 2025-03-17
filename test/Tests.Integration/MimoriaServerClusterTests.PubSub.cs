04a0daa1528df038dadb435ba79955772f3689d4
varelen <varelen@proton.me>
2025-03-17 19:27:16 +0100

    feat(Tests.Integration): Add server cluster tests for publish and subscribe

---
+++ test/Tests.Integration/MimoriaServerClusterTests.PubSub.cs
@@ -0,0 +1,93 @@
+﻿// SPDX-FileCopyrightText: 2025 varelen
+//
+// SPDX-License-Identifier: MIT
+
+using Varelen.Mimoria.Client;
+
+namespace Varelen.Mimoria.Tests.Integration;
+
+public partial class MimoriaServerClusterTests : IAsyncLifetime
+{
+    [Fact]
+    public async Task PubSub_Given_TwoNodes_When_SubscribeAndPublishString_Then_PayloadEventIsCalled()
+    {
+        // Arrange
+        await using var clusterMimoriaClient = await this.ConnectToClusterAsync();
+
+        string? value = null;
+
+        // Act
+        Subscription subscription = await clusterMimoriaClient.SubscribeAsync("test");
+        subscription.Payload += (payload) =>
+        {
+            value = payload;
+        };
+        await clusterMimoriaClient.PublishAsync("test", "value");
+
+        await Task.Delay(500);
+
+        // Assert
+        Assert.Equal("value", value);
+    }
+
+    [Fact]
+    public async Task PubSub_Given_TwoNodes_When_SubscribeAndPublishBytes_Then_PayloadEventIsCalled()
+    {
+        // Arrange
+        await using var clusterMimoriaClient = await this.ConnectToClusterAsync();
+
+        byte[]? value = null;
+
+        // Act
+        Subscription subscription = await clusterMimoriaClient.SubscribeAsync("test");
+        subscription.Payload += (payload) =>
+        {
+            value = payload;
+        };
+        await clusterMimoriaClient.PublishAsync("test", new byte[] { 1, 2, 3, 4 });
+
+        await Task.Delay(500);
+
+        // Assert
+        Assert.Equal([1, 2, 3, 4], value);
+    }
+
+    [Fact]
+    public async Task PubSub_Given_TwoNodes_When_SubscribeAndUnsubscribeAndPublish_Then_PayloadEventIsNotCalled()
+    {
+        // Arrange
+        await using var clusterMimoriaClient = await this.ConnectToClusterAsync();
+
+        string? value = null;
+
+        // Act
+        Subscription subscription = await clusterMimoriaClient.SubscribeAsync("test");
+        subscription.Payload += (payload) =>
+        {
+            value = payload;
+        };
+
+        await clusterMimoriaClient.UnsubscribeAsync("test");
+
+        await clusterMimoriaClient.PublishAsync("test", "value");
+
+        await Task.Delay(500);
+
+        // Assert
+        Assert.Null(value);
+    }
+
+    [Fact]
+    public async Task PubSub_Given_TwoNodes_When_PrimaryGoesDown_Then_NewLeaderIsPublishedInPrimaryChangedChannel()
+    {
+        await using var clusterMimoriaClient = await this.ConnectToClusterAsync();
+
+        Assert.Equal(2, clusterMimoriaClient.ServerId);
+
+        this.mimoriaServerTwo.Stop();
+
+        await Task.Delay(6_000);
+
+        Assert.Equal(1, clusterMimoriaClient.ServerId);
+    }
+}
---
+++ test/Tests.Integration/MimoriaServerClusterTests.cs
@@ -0,0 +1,107 @@
+﻿// SPDX-FileCopyrightText: 2025 varelen
+//
+// SPDX-License-Identifier: MIT
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.Options;
+
+using NSubstitute;
+
+using System.Net;
+using System.Net.Sockets;
+
+using Varelen.Mimoria.Client;
+using Varelen.Mimoria.Server;
+using Varelen.Mimoria.Server.Cache;
+using Varelen.Mimoria.Server.Options;
+using Varelen.Mimoria.Server.Protocol;
+using Varelen.Mimoria.Server.PubSub;
+
+namespace Varelen.Mimoria.Tests.Integration;
+
+public partial class MimoriaServerClusterTests : IAsyncLifetime
+{
+    private const string Password = "password";
+    private const string ClusterPassword = "clusterpassword";
+
+    private readonly ILoggerFactory loggerFactory;
+    private readonly ExpiringDictionaryCache cacheTwo;
+    private readonly IPubSubService pubSubServiceOne;
+    private readonly IPubSubService pubSubServiceTwo;
+
+    private ExpiringDictionaryCache cacheOne;
+    private MimoriaServer mimoriaServerOne = null!;
+    private MimoriaServer mimoriaServerTwo = null!;
+    private ushort firstPort;
+    private ushort firstClusterPort;
+    private ushort secondPort;
+    private ushort secondClusterPort;
+
+    public MimoriaServerClusterTests()
+    {
+        var optionsMock = Substitute.For<IOptionsMonitor<ConsoleLoggerOptions>>();
+        optionsMock.CurrentValue.Returns(new ConsoleLoggerOptions());
+
+        this.loggerFactory = Substitute.For<ILoggerFactory>();
+        this.cacheOne = new ExpiringDictionaryCache(NullLogger<ExpiringDictionaryCache>.Instance, Substitute.For<IPubSubService>(), TimeSpan.FromMinutes(5));
+        this.cacheTwo = new ExpiringDictionaryCache(NullLogger<ExpiringDictionaryCache>.Instance, Substitute.For<IPubSubService>(), TimeSpan.FromMinutes(5));
+        this.pubSubServiceOne = new PubSubService(NullLogger<PubSubService>.Instance);
+        this.pubSubServiceTwo = new PubSubService(NullLogger<PubSubService>.Instance);
+    }
+
+    public async Task InitializeAsync()
+    {
+        (this.mimoriaServerOne, this.mimoriaServerTwo, firstPort, firstClusterPort, secondPort, secondClusterPort) = await this.CreateClusterAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        this.mimoriaServerOne.Stop();
+        this.mimoriaServerTwo.Stop();
+        this.cacheOne.Dispose();
+        this.cacheTwo.Dispose();
+        return Task.CompletedTask;
+    }
+
+    private static (ushort Port, ushort ClusterPort) GetFreePorts()
+    {
+        using var tcpListenerOne = new TcpListener(IPAddress.Loopback, port: 0);
+        tcpListenerOne.Start();
+        int port = ((IPEndPoint)tcpListenerOne.LocalEndpoint).Port;
+
+        using var tcpListenerTwo = new TcpListener(IPAddress.Loopback, port: 0);
+        tcpListenerTwo.Start();
+        int clusterPort = ((IPEndPoint)tcpListenerTwo.LocalEndpoint).Port;
+
+        return ((ushort)port, (ushort)clusterPort);
+    }
+
+    protected async Task<IClusterMimoriaClient> ConnectToClusterAsync()
+    {
+        var mimoriaClient = new ClusterMimoriaClient(Password, [new IPEndPoint(IPAddress.Loopback, this.firstPort), new IPEndPoint(IPAddress.Loopback, this.secondPort)]);
+        await mimoriaClient.ConnectAsync();
+        return mimoriaClient;
+    }
+
+    private async Task<(MimoriaServer Secondary, MimoriaServer Primary, ushort PortOne, ushort ClusterPortOne, ushort PortTwo, ushort ClusterPortTwo)> CreateClusterAsync()
+    {
+        var (portOne, clusterPortOne) = GetFreePorts();
+        var (portTwo, clusterPortTwo) = GetFreePorts();
+
+        var optionsMock = Substitute.For<IOptionsMonitor<MimoriaOptions>>();
+        optionsMock.CurrentValue.Returns(new MimoriaOptions() { Password = Password, Port = portOne, Cluster = new MimoriaOptions.ClusterOptions() { Id = 1, Port = clusterPortOne, Password = ClusterPassword, Nodes = [new() { Id = 2, Host = "127.0.0.1", Port = clusterPortTwo }] } });
+
+        var mimoriaServerOne = new MimoriaServer(NullLogger<MimoriaServer>.Instance, this.loggerFactory, optionsMock, this.pubSubServiceOne, new MimoriaSocketServer(NullLogger<MimoriaSocketServer>.Instance), this.cacheOne);
+
+        var optionsMock2 = Substitute.For<IOptionsMonitor<MimoriaOptions>>();
+        optionsMock2.CurrentValue.Returns(new MimoriaOptions() { Password = Password, Port = portTwo, Cluster = new MimoriaOptions.ClusterOptions() { Id = 2, Port = clusterPortTwo, Password = ClusterPassword, Nodes = [new() { Id = 1, Host = "127.0.0.1", Port = clusterPortOne }] } });
+
+        var mimoriaServerTwo = new MimoriaServer(NullLogger<MimoriaServer>.Instance, this.loggerFactory, optionsMock2, this.pubSubServiceTwo, new MimoriaSocketServer(NullLogger<MimoriaSocketServer>.Instance), this.cacheTwo);
+
+        await Task.WhenAll(mimoriaServerOne.StartAsync(), mimoriaServerTwo.StartAsync());
+
+        return (mimoriaServerOne, mimoriaServerTwo, portOne, clusterPortOne, portTwo, clusterPortTwo);
+    }
+}
