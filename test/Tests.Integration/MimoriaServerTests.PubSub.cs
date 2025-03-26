6f6fe2cb60eac0d921d42dddf4f58a657415d5e3
varelen <varelen@proton.me>
2025-03-26 20:13:55 +0100

    feat(Test.Integration): Add pub-sub resubscribe test

---
+++ test/Tests.Integration/MimoriaServerTests.PubSub.cs
@@ -0,0 +1,48 @@
+﻿// SPDX-FileCopyrightText: 2025 varelen
+//
+// SPDX-License-Identifier: MIT
+
+using Varelen.Mimoria.Client;
+
+namespace Varelen.Mimoria.Tests.Integration;
+
+public partial class MimoriaServerTests : IAsyncLifetime
+{
+    [Fact]
+    public async Task PubSub_Given_MimoriaClient_When_SubscribeAndReconnectAndPublish_Then_PayloadEventIsCalled()
+    {
+        // Arrange
+        const string channel = "pubsub:test";
+        const string payload = "test";
+
+        await using var mimoriaClient = await this.ConnectToServerAsync();
+
+        string? receivedPayload = null;
+
+        Subscription subscription = await mimoriaClient.SubscribeAsync(channel);
+        subscription.Payload += (payload) =>
+        {
+            receivedPayload = payload;
+        };
+
+        // Act 1
+        this.mimoriaServerOne.Stop();
+
+        await Task.Delay(1_000);
+
+        await this.mimoriaServerOne.StartAsync();
+
+        // Assert 1
+        await Task.Delay(5_000);
+
+        Assert.True(mimoriaClient.IsConnected);
+
+        // Act 2
+        await mimoriaClient.PublishAsync(channel, payload);
+
+        await Task.Delay(500);
+
+        // Assert 2
+        Assert.Equal(payload, receivedPayload);
+    }
+}
