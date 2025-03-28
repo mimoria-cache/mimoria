6d35e3aacc30b548a47f4b01cb2ac42de0d0fdbf
varelen <varelen@proton.me>
2025-03-28 21:00:34 +0100

    feat(Tests.Integration): Improve server reconnect tests

--- test/Tests.Integration/MimoriaServerTests.Reconnect.cs
+++ test/Tests.Integration/MimoriaServerTests.Reconnect.cs
@@ -6,8 +6,12 @@ namespace Varelen.Mimoria.Tests.Integration;
 
 public partial class MimoriaServerTests : IAsyncLifetime
 {
-    [Fact]
-    public async Task Reconnect_Given_MimoriaClient_When_ServerRestartsAndAfterwardsSetAndGetString_Then_CorrectValueIsReturned()
+    [Theory]
+    [InlineData(1_000)]
+    [InlineData(2_000)]
+    [InlineData(5_000)]
+    [InlineData(10_000)]
+    public async Task Reconnect_Given_MimoriaClient_When_ServerRestartsAndAfterwardsSetAndGetString_Then_CorrectValueIsReturned(int serverDowntimeMs)
     {
         // Arrange
         const string key = "string:reconnect:key";
@@ -21,7 +25,7 @@ public partial class MimoriaServerTests : IAsyncLifetime
         // Act
         this.mimoriaServerOne.Stop();
 
-        await Task.Delay(1_000);
+        await Task.Delay(serverDowntimeMs);
 
         // Assert
         Assert.False(mimoriaClient.IsConnected);
@@ -33,6 +37,48 @@ public partial class MimoriaServerTests : IAsyncLifetime
 
         // Assert
         Assert.True(mimoriaClient.IsConnected);
+
+        // Act (verify it's working)
+        await mimoriaClient.SetStringAsync(key, value);
+        string? actualValue = await mimoriaClient.GetStringAsync(key);
+
+        // Assert
+        Assert.Equal(value, actualValue);
+    }
+
+    [Theory]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(5)]
+    public async Task Reconnect_Given_MimoriaClient_When_ServerRestartsAndReconnectAndRestartAfterwardsSetAndGetString_Then_CorrectValueIsReturned(int restartCount)
+    {
+        // Arrange
+        const string key = "string:reconnect:key";
+        const string value = "value";
+
+        await using var mimoriaClient = await this.ConnectToServerAsync();
+
+        // Assert
+        Assert.True(mimoriaClient.IsConnected);
+
+        for (int i = 0; i < restartCount; i++)
+        {
+            // Act
+            this.mimoriaServerOne.Stop();
+
+            await Task.Delay(1_000);
+
+            // Assert
+            Assert.False(mimoriaClient.IsConnected);
+
+            // Act
+            await this.mimoriaServerOne.StartAsync();
+
+            await Task.Delay(5_000);
+
+            // Assert
+            Assert.True(mimoriaClient.IsConnected);
+        }
 
         // Act (verify it's working)
         await mimoriaClient.SetStringAsync(key, value);
