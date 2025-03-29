22c70b0ab7b58b4b9e8e95a5f0c21a5433ca8b6d
varelen <varelen@proton.me>
2025-03-29 13:20:32 +0100

    feat(Tests.Integration): Add tests for supported replicated operations

--- test/Tests.Integration/MimoriaServerClusterTests.Replication.cs
+++ test/Tests.Integration/MimoriaServerClusterTests.Replication.cs
@@ -7,17 +7,81 @@ namespace Varelen.Mimoria.Tests.Integration;
 public partial class MimoriaServerClusterTests : IAsyncLifetime
 {
     [Fact]
-    public async Task Replication_Given_TwoNodes_When_WriteToPrimary_WithSyncReplication_Then_SecondaryAlsoHasKey()
+    public async Task Replication_Given_TwoNodes_When_SetString_WithSyncReplication_Then_SecondaryAlsoHasKey()
     {
         // Arrange
+        const string key = "key:replication:string";
+        const string value = "value";
+
         await using var clusterMimoriaClient = await this.ConnectToClusterAsync();
 
         // Act
-        await clusterMimoriaClient.SetStringAsync("key", "value");
+        await clusterMimoriaClient.SetStringAsync(key, value);
 
-        string? value = await clusterMimoriaClient.GetStringAsync("key", preferSecondary: true);
+        string? actualValue = await clusterMimoriaClient.GetStringAsync(key, preferSecondary: true);
 
         // Assert
-        Assert.Equal("value", value);
+        Assert.Equal(value, actualValue);
+    }
+
+    [Fact]
+    public async Task Replication_Given_TwoNodes_When_Delete_WithSyncReplication_Then_SecondaryDoesNotHaveKey()
+    {
+        // Arrange
+        const string key = "key:replication:delete";
+        const string value = "value";
+
+        await using var clusterMimoriaClient = await this.ConnectToClusterAsync();
+
+        // Act
+        await clusterMimoriaClient.SetStringAsync(key, value);
+        await clusterMimoriaClient.DeleteAsync(key);
+
+        string? actualValue = await clusterMimoriaClient.GetStringAsync(key, preferSecondary: true);
+
+        // Assert
+        Assert.Null(actualValue);
+    }
+
+    [Fact]
+    public async Task Replication_Given_TwoNodes_When_AddAndRemoveList_WithSyncReplication_Then_SecondaryHasCorrectValues()
+    {
+        // Arrange
+        const string key = "key:replication:delete";
+        string[] values = ["value1", "value2", "value3", "value4"];
+
+        await using var clusterMimoriaClient = await this.ConnectToClusterAsync();
+
+        // Act
+        await clusterMimoriaClient.AddListAsync(key, values[0]);
+        await clusterMimoriaClient.AddListAsync(key, values[1]);
+        await clusterMimoriaClient.AddListAsync(key, values[2]);
+        await clusterMimoriaClient.AddListAsync(key, values[3]);
+
+        await clusterMimoriaClient.RemoveListAsync(key, values[2]);
+
+        List<string> actualValues = await clusterMimoriaClient.GetListAsync(key, preferSecondary: true);
+
+        // Assert
+        Assert.Equal(3, actualValues.Count);
+        Assert.Equal(["value1", "value2", "value4"], actualValues);
+    }
+
+    [Fact]
+    public async Task Replication_Given_TwoNodes_When_SetBytes_WithSyncReplication_Then_SecondaryAlsoHasKey()
+    {
+        // Arrange
+        const string key = "key:replication:bytes";
+        byte[] value = [1, 3, 3, 7];
+
+        await using var clusterMimoriaClient = await this.ConnectToClusterAsync();
+
+        // Act
+        await clusterMimoriaClient.SetBytesAsync(key, value);
+
+        byte[]? actualValue = await clusterMimoriaClient.GetBytesAsync(key, preferSecondary: true);
+
+        // Assert
+        Assert.Equal(value, actualValue);
     }
 }
