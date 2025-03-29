f20005417ce84b797d64104c8863ad9f2bea2990
varelen <varelen@proton.me>
2025-03-29 13:03:39 +0100

    feat(Tests.Integration): Add set string cluster replication test

---
+++ test/Tests.Integration/MimoriaServerClusterTests.Replication.cs
@@ -0,0 +1,23 @@
+﻿// SPDX-FileCopyrightText: 2025 varelen
+//
+// SPDX-License-Identifier: MIT
+
+namespace Varelen.Mimoria.Tests.Integration;
+
+public partial class MimoriaServerClusterTests : IAsyncLifetime
+{
+    [Fact]
+    public async Task Replication_Given_TwoNodes_When_WriteToPrimary_WithSyncReplication_Then_SecondaryAlsoHasKey()
+    {
+        // Arrange
+        await using var clusterMimoriaClient = await this.ConnectToClusterAsync();
+
+        // Act
+        await clusterMimoriaClient.SetStringAsync("key", "value");
+
+        string? value = await clusterMimoriaClient.GetStringAsync("key", preferSecondary: true);
+
+        // Assert
+        Assert.Equal("value", value);
+    }
+}
