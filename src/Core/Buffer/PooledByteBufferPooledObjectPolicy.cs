99a5665465ec27d271b6945176afe85c161e5890
varelen <varelen@proton.me>
2025-04-03 20:48:43 +0200

    feat(Core): Add docs to pooled object policy

--- src/Core/Buffer/PooledByteBufferPooledObjectPolicy.cs
+++ src/Core/Buffer/PooledByteBufferPooledObjectPolicy.cs
@@ -6,11 +6,16 @@ using Microsoft.Extensions.ObjectPool;
 
 namespace Varelen.Mimoria.Core.Buffer;
 
+/// <summary>
+/// A policy for creating and returning <see cref="PooledByteBuffer"/> instances.
+/// </summary>
 public sealed class PooledByteBufferPooledObjectPolicy : IPooledObjectPolicy<PooledByteBuffer>
 {
+    /// <inheritdoc />
     public PooledByteBuffer Create()
         => new();
 
+    /// <inheritdoc />
     public bool Return(PooledByteBuffer pooledByteBuffer)
     {
         pooledByteBuffer.Reset();
