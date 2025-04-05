a5806f8c89b4c402dce987ab27307d4eb9704607
varelen <varelen@proton.me>
2025-04-05 22:00:16 +0200

    feat(Tests.System): Build docker image only once

--- test/Tests.System/MimoriaContainerFixture.cs
+++ test/Tests.System/MimoriaContainerFixture.cs
@@ -13,22 +13,16 @@ public sealed class MimoriaContainerFixture : IAsyncLifetime
     public const string Ip = "127.0.0.1";
     public const string Password = "tests";
 
-    private IFutureDockerImage image = null!;
     private IContainer container = null!;
 
     public IContainer Container => this.container;
 
     public async Task InitializeAsync()
     {
-        this.image = new ImageFromDockerfileBuilder()
-            .WithDockerfileDirectory(CommonDirectoryPath.GetSolutionDirectory(), string.Empty)
-            .WithDockerfile("src/Service/Dockerfile")
-            .Build();
-
-        await this.image.CreateAsync();
+        IFutureDockerImage image = await MimoriaDockerImage.GetOrCreateImageAsync();
 
         this.container = new ContainerBuilder()
-            .WithImage(this.image)
+            .WithImage(image)
             .WithEnvironment("MIMORIA__PASSWORD", Password)
             .WithPortBinding(6565, assignRandomHostPort: true)
             .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(6565))
@@ -40,7 +34,5 @@ public sealed class MimoriaContainerFixture : IAsyncLifetime
     public async Task DisposeAsync()
     {
         await this.container.DisposeAsync();
-        await this.image.DeleteAsync();
-        await this.image.DisposeAsync();
     }
 }
---
+++ test/Tests.System/MimoriaDockerImage.cs
@@ -0,0 +1,30 @@
+﻿// SPDX-FileCopyrightText: 2025 varelen
+//
+// SPDX-License-Identifier: MIT
+
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Images;
+
+namespace Varelen.Mimoria.Tests.System;
+
+public static class MimoriaDockerImage
+{
+    private static IFutureDockerImage? image = null;
+
+    public static async Task<IFutureDockerImage> GetOrCreateImageAsync()
+    {
+        if (image is not null)
+        {
+            return image;
+        }
+
+        image = new ImageFromDockerfileBuilder()
+            .WithDockerfileDirectory(CommonDirectoryPath.GetSolutionDirectory(), string.Empty)
+            .WithDockerfile("src/Service/Dockerfile")
+            .Build();
+
+        await image.CreateAsync();
+
+        return image;
+    }
+}
