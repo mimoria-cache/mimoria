a61fa25f29de6ce9c2eeb995fa9e2c3188e8c023
varelen <varelen@proton.me>
2025-05-01 20:18:18 +0200

    chore(Client): Add remaining docs to custom exceptions

--- src/Client/Exceptions/NoPrimaryAvailableException.cs
+++ src/Client/Exceptions/NoPrimaryAvailableException.cs
@@ -9,14 +9,27 @@ namespace Varelen.Mimoria.Client.Exceptions;
 /// </summary>
 public sealed class NoPrimaryAvailableException : InvalidOperationException
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NoPrimaryAvailableException"/> class.
+    /// </summary>
     public NoPrimaryAvailableException()
     {
     }
 
-    public NoPrimaryAvailableException(string? message) : base(message)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NoPrimaryAvailableException"/> class with a specified error message.
+    /// </summary>
+    /// <param name="message">The error message.</param>
+    public NoPrimaryAvailableException(string? message)
+        : base(message)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NoPrimaryAvailableException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <param name="message">The error message.</param>
+    /// <param name="innerException">The inner exception.</param>
     public NoPrimaryAvailableException(string? message, Exception? innerException)
         : base(message, innerException)
     {
--- src/Client/Exceptions/NoSecondaryAvailableException.cs
+++ src/Client/Exceptions/NoSecondaryAvailableException.cs
@@ -9,14 +9,27 @@ namespace Varelen.Mimoria.Client.Exceptions;
 /// </summary>
 public sealed class NoSecondaryAvailableException : InvalidOperationException
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NoSecondaryAvailableException"/> class.
+    /// </summary>
     public NoSecondaryAvailableException()
     {
     }
 
-    public NoSecondaryAvailableException(string? message) : base(message)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NoSecondaryAvailableException"/> class with a specified error message.
+    /// </summary>
+    /// <param name="message">The error message.</param>
+    public NoSecondaryAvailableException(string? message)
+        : base(message)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NoSecondaryAvailableException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <param name="message">The error message.</param>
+    /// <param name="innerException">The inner exception.</param>
     public NoSecondaryAvailableException(string? message, Exception? innerException)
         : base(message, innerException)
     {
