d7dc08509e2b7e0afdfd96b26be544ad5cd1a4c6
varelen <varelen@proton.me>
2025-05-01 19:23:54 +0200

    feat(Core): Use string pool for channels that are built with a template and a key

--- src/Core/Channels.cs
+++ src/Core/Channels.cs
@@ -2,6 +2,9 @@
 //
 // SPDX-License-Identifier: MIT
 
+using CommunityToolkit.HighPerformance.Buffers;
+
+using System.Buffers;
 using System.Runtime.CompilerServices;
 
 namespace Varelen.Mimoria.Core;
@@ -11,6 +14,16 @@ namespace Varelen.Mimoria.Core;
 /// </summary>
 public static class Channels
 {
+    /// <summary>
+    /// The length of the placeholder in the template strings.
+    /// </summary>
+    public const int PlaceholderLength = 3;
+
+    /// <summary>
+    /// The maximum length of the buffer used for stack allocation.
+    /// </summary>
+    public const int MaxStackAllocBufferLength = 256;
+
     /// <summary>
     /// Published to if a key expired. Payload is the key (string) which expired.
     /// </summary>
@@ -36,7 +49,6 @@ public static class Channels
     /// </summary>
     public const string ListAddedTemplate = "__key:{0}:list:added";
 
-    // TODO: Cache results, it could be used quite often on the server?#
     /// <summary>
     /// Returns the channel for when a list item was added to the list at the specified key.
     /// </summary>
@@ -44,5 +56,37 @@ public static class Channels
     /// <returns>The channel for when a list item was added to the list at the specified key.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static string ForListAdded(string key)
-        => string.Format(ListAddedTemplate, key);
+        => GetFullChannel(ListAddedTemplate, key);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static string GetFullChannel(string channelTemplate, string key)
+    {
+        int totalLength = channelTemplate.Length - PlaceholderLength + key.Length;
+        
+        char[]? rentedBuffer = null;
+
+        Span<char> buffer = totalLength <= MaxStackAllocBufferLength
+            ? stackalloc char[totalLength]
+            : (rentedBuffer = ArrayPool<char>.Shared.Rent(totalLength));
+
+        try
+        {
+            int templatePlaceholderIndex = channelTemplate.AsSpan().IndexOf("{0}");
+
+            channelTemplate.AsSpan(0, templatePlaceholderIndex).CopyTo(buffer);
+
+            key.AsSpan().CopyTo(buffer[templatePlaceholderIndex..]);
+
+            channelTemplate.AsSpan(templatePlaceholderIndex + PlaceholderLength).CopyTo(buffer[(templatePlaceholderIndex + key.Length)..]);
+
+            return StringPool.Shared.GetOrAdd(buffer[..totalLength]);
+        }
+        finally
+        {
+            if (rentedBuffer is not null)
+            {
+                ArrayPool<char>.Shared.Return(rentedBuffer);
+            }
+        }
+    }
 }
